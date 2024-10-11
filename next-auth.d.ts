75df5b8ddcc3cfbf25d618c09d08a789c204eaa0
Nathan Carlini <nathan1.carlini@gmail.com>
2024-10-11 13:03:07 +0200

    moved away goog tkn verif

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,39 +1,3 @@
-// import NextAuth, { User, type DefaultSession } from "next-auth";
-
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id: string;
-//       token: string;
-//       name: string;
-//       phone: string;
-//       role: string;
-//       picture: string;
-//     };
-//   }
-//   interface User {
-//     id: string;
-//     token: string;
-//     name: string;
-//     phone: string;
-//     role: string;
-//     picture: string;
-//   }
-// }
-
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     user: {
-//       id: string;
-//       token: string;
-//       name: string;
-//       phone: string;
-//       role: string;
-//       picture: string;
-//     };
-//   }
-// }
-
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
@@ -49,5 +13,6 @@ declare module "next-auth" {
     id: string;
     // Ajoute d'autres propriétés si nécessaire
   }
+  function verifyGoogleToken()
 }
 
