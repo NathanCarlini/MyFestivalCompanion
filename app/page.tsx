5a23e6c114bc0eb3964e3ffd6b9f5beb0bda78fc
Nathan Carlini <nathan1.carlini@gmail.com>
2024-10-11 13:00:23 +0200

    removed test logins

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-"use client"
-import { signIn } from 'next-auth/react';
 
-const SignInPage = () => {
-  return (
-    <div>
-      <h1>Sign In</h1>
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-      <button onClick={() => signIn('facebook')}>Sign in with Facebook</button>
-      <button onClick={() => signIn('apple')}>Sign in with Apple</button>
-      <button onClick={() => signIn('email')}>Sign in with Email</button>
-    </div>
+import { getfestivals } from "@/app/api/getfestivals/route";
+
+export default async function Home() {
+const data = await getfestivals();
+return (
+<main>
+<p className="text-red-500">bonjour</p>
+</main>
   );
-};
-
-export default SignInPage;
+} 
